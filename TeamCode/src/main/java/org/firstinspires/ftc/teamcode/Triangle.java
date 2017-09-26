d16fbce3f3f6ebd7067039126586100be952d560
Gigel Avram <gigela@hotmail.com>
2017-09-26 15:21:46 -0700

    updated op2 - should be ready to test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Triangle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Triangle.java
@@ -6,24 +6,65 @@ package org.firstinspires.ftc.teamcode;
 
 public class Triangle {
 
-    public double a1;
-    public double a2;
-    public double a3;
+    public double a1 = 0;
+    public double a2 = 0;
+    public double a3 = 0;
 
-    public double l1;
-    public double l2;
-    public double l3;
+    public double l1 = 0;
+    public double l2 = 0;
+    public double l3 = 0;
+
+    public boolean isValid = false;
 
     /* Constructor */
-    public void SafeServo(){
+    public void Triangle(){
     }
 
-    public void init_l1a2a3( double l1, double a2, double a3 ){
+    public void init_l1a2a3( double al1, double aa2, double aa3 ){
+
+        l1 = al1;
+
+        a2 = aa2;
+        a3 = aa3;
+        a1 = Math.PI - a2 - a3;
+
+        l2 = l1 * Math.sin( a2 ) / Math.sin( a1 );
+        l3 = l1 * Math.sin( a3 ) / Math.sin( a1 );
+
+        isValid = true;
     }
 
-    public void init_l1l2a3( double l1, double l2, double a3 ){
+    public void init_l1l2a3( double al1, double al2, double aa3 ) {
+
+        l1 = al1;
+        l2 = al2;
+
+        a3 = aa3;
+
+        l3 = Math.sqrt(l2 * l2 + l1 * l1 - 2 * l2 * l1 * Math.cos(a3));
+
+        if (l1 > l2) {
+            a2 = Math.asin( l2 * Math.sin( a3 ) / l3 );
+            a1 = Math.PI - a2 - a3;
+        }
+        if (l2 > l1) {
+            a1 = Math.asin( l1 * Math.sin( a3 ) / l3 );
+            a2 = Math.PI - a1 - a3;
+        }
+
+        isValid = true;
     }
 
-    public void init_l1l2l3( double l1, double l2, double l3 ){
+    public void init_l1l2l3( double al1, double al2, double al3 ){
+
+        l1 = al1;
+        l2 = al2;
+        l3 = al3;
+
+        a1 = Math.acos( ( l2 * l2 + l3 * l3 - l1 * l1 ) / ( 2 * l2 * l3 ) );
+        a2 = Math.acos( ( l1 * l1 + l3 * l3 - l2 * l2 ) / ( 2 * l2 * l3 ) );
+        a3 = Math.PI - a1 - a2;
+
+        isValid = true;
     }
 }
