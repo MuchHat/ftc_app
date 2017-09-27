1a4c70fde6452fa4c7a3e8239f24259bf6491a37
MuchHat <gigela@Hotmail.com>
2017-09-27 06:39:20 -0700

    working version for test xbox pad, moveServo smooth not working, using Thread.sleep

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Point.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by gigel on 2017-09-27.
+ */
+
+public class Point {
+
+    public double ax = 0;
+    public double ay = 0;
+    public double az = 0;
+
+    public double x = 0;
+    public double y = 0;
+    public double z = 0;
+
+    public double r = 0;
+
+    public boolean isValid = false;
+
+    /* Constructor */
+    public void Point(){
+    }
+
+    public void resolve_SAA( double al1, double aa2, double aa3 ) {
+
+        l1 = al1;
+    }
+}
