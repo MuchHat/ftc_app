85a102fa520a4c14974e3243075a22d1797ee443
Gigel Avram <gigela@hotmail.com>
2017-10-04 12:31:13 -0700

    fixed telemetry to show %

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Angle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Angle.java
@@ -11,7 +11,7 @@ import com.qualcomm.robotcore.util.Range;
 public class Angle {
 
     public double angleServo = 0.0; // between 0 and 1.0
-    public double anglePI = 0.0;  // between -0to pi
+    public double anglePI = 0.0;  // between -0 to pi
 
     public double offsetServo = 0.0; // default 0.0
     public double slopeServo = 1.0; // default 1.0
@@ -22,38 +22,42 @@ public class Angle {
 
     }
 
-    public void Init( double angle1, double angle2, double pos1, double pos2, double aMin, double aMax ) {
+    public void Init(double angle1, double angle2, double pos1, double pos2, double aMin, double aMax) {
 
         // angle1 and angle2 are the observed angles for pos1 and pos2 -
         // angle1 and angle2 are in 0.0->1.1
 
-        slopeServo = ( angle2 - angle1 ) / ( pos2 - pos1 );
-        offsetServo = angle1 - ( pos1 * slopeServo );
+        slopeServo = (angle2 - angle1) / (pos2 - pos1);
+        offsetServo = angle1 - (pos1 * slopeServo);
         minServo = aMin;
         maxServo = aMax;
     }
 
-    public double getServo(){ return angleServo; }
+    public double getServo() {
+        return angleServo;
+    }
 
-    public double getPI(){ return anglePI; }
+    public void setServo(double as) {
 
-    public void setServo( double as ) {
+        angleServo = Range.clip(as, minServo, maxServo);
 
-        angleServo = Range.clip( as, minServo, maxServo );
+        anglePI = (angleServo * slopeServo + offsetServo) * Math.PI;
 
-        anglePI = ( angleServo * slopeServo + offsetServo ) * Math.PI;
-
-        anglePI = Range.clip( anglePI, 0, Math.PI );
+        anglePI = Range.clip(anglePI, 0, Math.PI);
 
     }
 
-    public void setPI( double ap ) {
+    public double getPI() {
+        return anglePI;
+    }
 
-        anglePI = Range.clip( ap, 0, Math.PI );
+    public void setPI(double ap) {
 
-        angleServo = ( anglePI / Math.PI  - offsetServo ) / slopeServo;
+        anglePI = Range.clip(ap, 0, Math.PI);
 
-        angleServo = Range.clip( angleServo, minServo, maxServo );
+        angleServo = (anglePI / Math.PI - offsetServo) / slopeServo;
+
+        angleServo = Range.clip(angleServo, minServo, maxServo);
 
     }
 }
