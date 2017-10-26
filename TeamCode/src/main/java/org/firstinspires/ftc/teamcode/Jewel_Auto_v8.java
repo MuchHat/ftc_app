0d7b7f8bb816b4a6827ef7a4461a86f5273cc369
Gigel Avram <gigela@hotmail.com>
2017-10-26 15:53:34 -0700

    added placeholder for auto ops for move to safe and knock jewel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jewel_Auto_v8.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jewel_Auto_v8.java
@@ -96,7 +96,7 @@ public class Jewel_Auto_v8 extends LinearOpMode {
 
             int findPositions = 4;
             robot.moveArm(armExtendedB, armExtendedE);
-            waitMillis(555);
+            waitMillis(222);
 
             for (int i = 0; i < findPositions; i++) {
 
@@ -109,7 +109,7 @@ public class Jewel_Auto_v8 extends LinearOpMode {
                 if (foundJewel()) {
                     foundPos = i;
                     robot.moveArm(armExtendedB, armExtendedE);
-                    waitMillis(555);
+                    waitMillis(222);
                     break;
                 }
             }
@@ -125,21 +125,33 @@ public class Jewel_Auto_v8 extends LinearOpMode {
             if (foundBlue || foundRed) {
 
                 //TODO add logic to account for fw/rev and team color
+                boolean knockFirst = true; //knowck the ball in front or the other one
+                if (blueTeam && foundBlue) knockFirst = false;
+                if (!blueTeam && foundRed) knockFirst = false;
 
-                robot.move(11);
-                waitMillis(222);
-
+                if (!knockFirst) { // move to between the balls
+                    robot.move(11);
+                    waitMillis(222);
+                }
                 double crrBase = armKnockB[foundPos];
                 double crrElbow = armKnockE[foundPos];
                 robot.moveArm(crrBase, crrElbow);
                 waitMillis(222);
 
-                robot.move(-11);
-                waitMillis(222);
+                if (knockFirst) { // if knowcking the ball first right in front move backwards
+                    robot.move(-55);
+                    waitMillis(222);
+                    robot.move(55);
+                    waitMillis(222);
+                }
+                if (!knockFirst) { // else move forward
+                    robot.move(55);
+                    waitMillis(222);
+                    robot.move(-55);
+                    waitMillis(222);
+                }
 
-                robot.move(11);
-                waitMillis(222);
-
+                robot.moveArm(armExtendedB, armExtendedE);
                 robot.moveArm(robot.armPosZero[0], robot.armPosZero[1]);
                 waitMillis(222);
 
